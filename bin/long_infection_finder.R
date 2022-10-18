5f7f92f553eb4eaa91c377193dc3e3ebb0db1b97
Nicholas Minor <nrminor@wisc.edu>
2022-10-18 12:56:33 -0500

    small fix to long infection finder

--- bin/long_infection_finder.R
+++ bin/long_infection_finder.R
@@ -9,9 +9,6 @@ experiment_date <- as.Date(args[2]) ; experiment_date
 lineage_csv <- read.csv(args[3])
 dates <- read.csv(args[4])
 
-# ensuring dates are properly formatted
-dates$designation_date <- as.Date(dates$designation_date)
-
 # preparing a data frame to hold long infection data, if detected
 long_infections <- data.frame(sample = NA,
                               lineage = NA,
@@ -20,40 +17,55 @@ long_infections <- data.frame(sample = NA,
                               experiment_name = NA,
                               pango_version = NA)
 
-# diff <- c()
-for (i in 1:nrow(lineage_csv)){
+# Making sure the lineage csv isn't empty
+if (nrow(lineage_csv) != 0){
   
-  lineage <- lineage_csv$lineage[i]
-  designation_date <- dates[dates$lineage==lineage, 2]
+  # ensuring dates are properly formatted
+  dates$designation_date <- as.Date(dates$designation_date)
   
-  # diff <- c(diff, as.numeric(experiment_date - designation_date))
-  # mean(diff) ; hist(diff, breaks = 20)
-  
-  if ( as.numeric(experiment_date - designation_date) >= 240 ){
+  # diff <- c()
+  for (i in 1:nrow(lineage_csv)){
     
-    new_row <- c(lineage_csv$taxon[i],
-                 lineage,
-                 as.character(designation_date),
-                 as.character(experiment_date),
-                 experiment_name,
-                 lineage_csv$pangolin_version[i])
-    long_infections <- rbind(long_infections, new_row)
+    lineage <- lineage_csv$lineage[i]
+    designation_date <- dates[dates$lineage==lineage, 2]
     
-  } else {
-    next
+    if ( is.na(designation_date) ) {
+      designation_date <- as.Date("2021-02-18")
+    }
+    
+    # diff <- c(diff, as.numeric(experiment_date - designation_date))
+    # mean(diff) ; hist(diff, breaks = 20)
+    
+    if ( as.numeric(experiment_date - designation_date) >= 240 ){
+      
+      new_row <- c(lineage_csv$taxon[i],
+                   lineage,
+                   as.character(designation_date),
+                   as.character(experiment_date),
+                   experiment_name,
+                   lineage_csv$pangolin_version[i])
+      long_infections <- rbind(long_infections, new_row)
+      
+    } else {
+      next
+    }
+    
+    
+  }
+  if ( nrow(long_infections) > 1){
+    long_infections <- long_infections[2:nrow(long_infections),]
+    rownames(long_infections) <- NULL
   }
   
-  
 }
-long_infections <- long_infections[2:nrow(long_infections),]
-rownames(long_infections) <- NULL
 
-if (nrow(long_infections)==0){
+if (nrow(long_infections)==1 &&
+    is.na(long_infections[1, "sample"])){
   
   null_row <- rep(NA, times = ncol(long_infections))
   null_row[1] <- paste("No putative long infections were identified in experiment",
                        experiment_name, "on", Sys.Date(), sep = " ")
-  long_infections <- rbind(long_inf_table, null_row)
+  long_infections[1,] <- null_row
   
 }
 
